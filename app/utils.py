a661d191e24fae6ed7ffe8f9c2bb358cdef50865
yamatin_nikita <yamatin_nikita@cybersec74.ru>
2025-09-22 20:19:28 +0500

    remove logs files, fix gitignore

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-import ipaddress
-
-
-def is_valid_ipv4(ip: str) -> bool:
-    try:
-        ipaddress.IPv4Address(ip)
-        return True
-    except ipaddress.AddressValueError:
-        return False
